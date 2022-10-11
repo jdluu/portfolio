e35cd3ff45e5de527d2fa2db78c64dc4cc3a9ddf
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2022-10-11 14:35:19 -0700

    create base

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Works from "./components/Works";
+
+function App() { 
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Works />
+      <Contact />
+    </div>
+  );
+}
+export default App;
