7764fc1e8c11ad82a2850ad70c8f45b337f3f99e
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2022-10-13 11:34:06 -0700

    wrote functions for responsive react design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Works from "./components/Works";
+
+const isMobileDevice = ({
+  query: "(min-device-width: 480px)",
+});
+
+const isTabletDevice = ({
+  query: "(min-device-width: 768px)",
+});
+
+const isLaptopDevice = ({
+  query: "(min-device-width: 1024px)",
+});
+
+const isDesktopDevice = ({
+  query: "(min-device-width: 1200px)",
+});
+
+const isBigScreenDevice = ({
+  query: "(min-device-width: 1201px)",
+});
+
+
 
 function App() { 
   return (
     <div>
       <Navbar />
       <Home />
-      <About />
       <Works />
       <Contact />
     </div>
