6f0c57cffc2c4513076c8849336a49934d9274f9
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2022-12-12 15:15:56 -0800

    Rewrote tailwind into css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { ThemeContext } from '../../Theme';
+import './Contact.css';
+import '../../App.css';
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <div className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8 flex flex-col justify-center w-full h-full items-center'>
-                <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Send me a message</p>
-            </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+    <div class = "div1" >
+      <div class = "div2">
+        <div class = "div3">
+          <p class = "p1">Contact</p>
+          <p class = "p2">Feel free to reach out to me!</p>
         </div>
+        <input class = "input1" type = "text" placeholder = "Name" />
+        <input class = "input2" type = "text" placeholder = "Email" />
+        <textarea class = "textarea1" placeholder = "Message" />
+        <button class = "button1">Send</button>
+      </div>
     </div>
-  )
+  );
 }
 export default Contact;
