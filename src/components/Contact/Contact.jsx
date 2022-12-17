78a9040879f16070f12b6f9f1b06b704ec712094
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2022-12-16 16:43:41 -0800

    re-wrote contact and contact css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,16 +5,16 @@ import '../../App.css';
 
 const Contact = () => {
   return (
-    <div class = "div1" >
-      <div class = "div2">
-        <div class = "div3">
-          <p class = "p1">Contact</p>
-          <p class = "p2">Feel free to reach out to me!</p>
+    <div class = "ContactBox" >
+      <div class = "ContactParentColumn">
+        <div class = "ContactChildColumn">
+          <p class = "ContactHeader">Contact</p>
+          <p class = "ContactDescription">Feel free to reach out to me!</p>
         </div>
-        <input class = "input1" type = "text" placeholder = "Name" />
-        <input class = "input2" type = "text" placeholder = "Email" />
-        <textarea class = "textarea1" placeholder = "Message" />
-        <button class = "button1">Send</button>
+        <input class = "NameInput" type = "text" placeholder = "Name" />
+        <input class = "EmailInput" type = "text" placeholder = "Email" />
+        <textarea class = "MessageInput" placeholder = "Message" />
+        <button class = "SendButton">Send</button>
       </div>
     </div>
   );
